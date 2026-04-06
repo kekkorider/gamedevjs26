326786be7c39996cf1dfa7cdb26a4bd29c720a66
Francesco Michelini <kekkorider@users.noreply.github.com>
2026-04-06 22:52:48 +0200

    Minor edits to MainMenu

--- src/game/main.ts
+++ src/game/main.ts
@@ -19,7 +19,11 @@ const config: Phaser.Types.Core.GameConfig = {
         MainMenu,
         MainGame,
         GameOver
-    ]
+    ],
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
 };
 
 const StartGame = (parent: string) => {
--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -6,8 +6,7 @@ export class MainMenu extends Scene
 {
     background: GameObjects.Image;
     logo: GameObjects.Image;
-    title: GameObjects.Text;
-    logoTween: Phaser.Tweens.Tween | null;
+    button: GameObjects.Text;
 
     constructor ()
     {
@@ -16,61 +15,36 @@ export class MainMenu extends Scene
 
     create ()
     {
-        this.background = this.add.image(512, 384, 'background');
+        this.background = this.add.image(this.scale.width / 2, 384, 'background');
 
-        this.logo = this.add.image(512, 300, 'logo').setDepth(100);
+        this.logo = this.add.image(this.scale.width / 2, 300, 'logo').setDepth(100);
 
-        this.title = this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
+        this.button = this.add.text(
+            this.scale.width / 2,
+            this.scale.height / 2 + 30,
+            'Start Game',
+            {
+                fontFamily: 'Arial Black',
+                fontSize: 38,
+                color: '#ffffff',
+                align: 'center',
+                backgroundColor: '#000000',
+            }
+        )
+        .setOrigin(0.5)
+        .setDepth(100)
+        .setPadding(16);
+
+        this.button.setInteractive({ useHandCursor: true });
+
+        this.button.on('pointerdown', () => {
+            this.changeScene();
+        })
 
         EventBus.emit('current-scene-ready', this);
     }
-    
-    changeScene ()
-    {
-        if (this.logoTween)
-        {
-            this.logoTween.stop();
-            this.logoTween = null;
-        }
 
+    changeScene (){
         this.scene.start('Game');
-    }
-
-    moveLogo (vueCallback: ({ x, y }: { x: number, y: number }) => void)
-    {
-        if (this.logoTween)
-        {
-            if (this.logoTween.isPlaying())
-            {
-                this.logoTween.pause();
-            }
-            else
-            {
-                this.logoTween.play();
-            }
-        } 
-        else
-        {
-            this.logoTween = this.tweens.add({
-                targets: this.logo,
-                x: { value: 750, duration: 3000, ease: 'Back.easeInOut' },
-                y: { value: 80, duration: 1500, ease: 'Sine.easeOut' },
-                yoyo: true,
-                repeat: -1,
-                onUpdate: () => {
-                    if (vueCallback)
-                    {
-                        vueCallback({
-                            x: Math.floor(this.logo.x),
-                            y: Math.floor(this.logo.y)
-                        });
-                    }
-                }
-            });
-        }
     }
 }
